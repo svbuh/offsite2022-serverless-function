119c29480597fe002f40a720a1ebc220061b8404
Buhre, Sven <sven.buhre@bdr.de>
2022-09-15 08:18:54 +0200

    first commit

--- functions/offsite22.ts
+++ functions/offsite22.ts
@@ -1,50 +1,18 @@
 import { HandlerEvent } from '@netlify/functions';
 
-type Pizza = {
-    type: 'pizza';
-    sauce: 'red' | 'white';
-    /**
-     * Things like pepperoni, mushrooms, etc.
-     */
-    toppings?: string[];
-};
+function schedulePlan(timeslot: string): string {
+    switch (timeslot) {
+        case 'thu_3pm':
+            return `Welcome! It's time to check-in`;
 
-type HotDog = {
-    type: 'hotdog';
-    /**
-     * @deprecated all hot dogs are now footlong
-     */
-    size: 'ball park' | 'footlong';
-    /**
-     * Things like relish, onions, etc.
-     */
-    toppings?: string[];
-};
+        case 'thu_9pm':
+            return `Drinks & Live music @Havanabar`;
 
-type Taco = {
-    type: 'taco';
-    spiceLevel: 'none' | 'mild' | 'hot';
-    /**
-     * Things like salsa, cheese, etc.
-     */
-    toppings?: string[];
-};
+        case 'fri_5:30pm':
+            return `Team photo!`;
 
-type Sandwich = Pizza | HotDog | Taco;
-
-function serveSandwich(sandwich: Sandwich): string {
-    switch (sandwich.type) {
-        case 'pizza':
-            sandwich.toppings;
-            return `Pizza with ${sandwich.sauce} sauce`;
-
-        case 'hotdog':
-            sandwich.toppings;
-            return `Hot dog with ${sandwich.size} size`;
-
-        case 'taco':
-            sandwich.toppings;
-            return `Taco with ${sandwich.spiceLevel} spice`;
+        case 'sat_07:30am':
+            return `Breakfast @SeaPoint`;
 
         // this is not reachable due to TS checks
         default:
@@ -54,27 +22,12 @@ function serveSandwich(sandwich: Sandwich): string {
 
 export const handler = async (event: HandlerEvent) => {
     try {
-        const type = event?.queryStringParameters?.type || 'pizza';
+        const timeslot = event?.queryStringParameters?.schedule || 'thu_3pm';
 
-        const pizza: Pizza = {
-            type: 'pizza',
-            sauce: 'red',
-        };
-        const hotdog: HotDog = {
-            type: 'hotdog',
-            size: 'ball park',
-        };
-        const taco: Taco = {
-            type: 'taco',
-            spiceLevel: 'hot',
-        };
-
-        const sandwich =
-            type === 'pizza' ? pizza : type === 'hotdog' ? hotdog : taco;
 
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: serveSandwich(sandwich) }),
+            body: JSON.stringify({ message: schedulePlan(timeslot) }),
         };
     } catch (error: any) {
         return { statusCode: 500, body: error.toString() };
